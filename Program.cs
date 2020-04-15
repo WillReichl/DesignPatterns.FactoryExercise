05f1b1f062306d8e31969dc62af8eac321485d59
Will Reichl <William.Reichl@dcsg.com>
2020-04-14 23:37:45 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FactoryExercise
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
