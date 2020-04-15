70100f6698c6cfd00975c34250e8830d9f631fa2
Will Reichl <William.Reichl@dcsg.com>
2020-04-15 07:23:06 -0400

    Solution (non-static Factory that tracks # people created)

--- Program.cs
+++ Program.cs
@@ -6,13 +6,37 @@ namespace FactoryExercise
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"My name is {Name} and my ID is {Id}.";
+        }
+    }
+
+    public class PersonFactory
+    {
+        private int PeopleCreated;
+        public PersonFactory()
+        {
+            this.PeopleCreated = 0;
+        }
+
+        public Person CreatePerson(string personName)
+        {
+            var person =  new Person { Id = this.PeopleCreated, Name = personName };
+            this.PeopleCreated++;
+            return person;
+        }
     }
 
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pf = new PersonFactory();
+            var jack = pf.CreatePerson("Jack");
+            var jill = pf.CreatePerson("Jill");
+            Console.WriteLine(jack);
+            Console.WriteLine(jill);
         }
     }
 }
